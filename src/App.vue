15c85367b30d0ed139ffcc1f7a4204a6b5dec72f
Jasper <well.caffeinated@gmail.com>
2020-12-01 16:25:24 -0700

    switch to vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-#nav
-  router-link(to="/") Home
-  router-link(to="/about") About
-router-view
+#app
+  #nav
+    router-link(to="/") Home
+    router-link(to="/about") About
+  router-view
 </template>
 
 <style lang="sass">
